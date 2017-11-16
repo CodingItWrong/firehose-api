55e927ad0c7ec996e7cbb52c949c69e35f42d261
Josh Justice <jjustice@bignerdranch.com>
2017-11-16 06:01:50 -0500

    Add acts-as-taggable-on

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'devise'
+gem 'acts-as-taggable-on'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
---
+++ config/initializers/acts_as_taggable_on.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class SpaceParser < ActsAsTaggableOn::GenericParser
+  def parse
+    ActsAsTaggableOn::TagList.new.tap do |tag_list|
+      tag_list.add @tag_list.split(' ')
+    end
+  end
+end
+
+ActsAsTaggableOn.default_parser = SpaceParser
+ActsAsTaggableOn.remove_unused_tags = true
