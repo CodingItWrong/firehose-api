62db959176d05739d1e856a907bd611f148d6cf4
Josh Justice <jjustice@bignerdranch.com>
2017-11-20 05:58:55 -0500

    Get public tag page working

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,9 +3,19 @@
 class TagsController < ApplicationController
   def show
     @tag = ActsAsTaggableOn::Tag.where(name: params[:id]).first
-    @links = Link.joins(:tags)
-                 .where('tags.id' => @tag.id)
-                 .newest
-                 .unread
+    @links = links_for_current_user
+  end
+
+  private
+
+  def links_for_current_user
+    links = Link.joins(:tags)
+                .where('tags.id' => @tag.id)
+                .newest
+    if user_signed_in?
+      links.unread
+    else
+      links.publicly_visible
+    end
   end
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -5,9 +5,16 @@ require 'rails_helper'
 RSpec.feature 'Viewing Links', type: :feature do
   let!(:public_links) { FactoryBot.create_list(:link, 3, :public) }
   let!(:private_links) { FactoryBot.create_list(:link, 3, :private) }
+  let!(:public_tagged_links) {
+    FactoryBot.create_list(:link, 3, :public, tag_list: 'foo')
+  }
+  let!(:private_tagged_links) {
+    FactoryBot.create_list(:link, 3, :private, tag_list: 'foo')
+  }
 
   it 'displays public links' do
     view_links_page
+    view_tag_page
   end
 
   def view_links_page
@@ -21,5 +28,16 @@ RSpec.feature 'Viewing Links', type: :feature do
     end
     expect(page).not_to have_content('Add')
     expect(page).not_to have_content('Edit')
+  end
+
+  def view_tag_page
+    visit '/tags/foo'
+
+    public_tagged_links.each do |link|
+      expect(page).to have_content(link.title)
+    end
+    private_tagged_links.each do |link|
+      expect(page).not_to have_content(link.title)
+    end
   end
 end
