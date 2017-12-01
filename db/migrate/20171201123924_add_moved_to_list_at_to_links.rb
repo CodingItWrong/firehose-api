821ed9368c066558c075b4159d8dd2cd4a4b5cf5
Josh Justice <jjustice@bignerdranch.com>
2017-12-01 08:11:26 -0500

    Preserve read dates

--- db/migrate/20171201123924_add_moved_to_list_at_to_links.rb
+++ db/migrate/20171201123924_add_moved_to_list_at_to_links.rb
@@ -1,5 +1,15 @@
 class AddMovedToListAtToLinks < ActiveRecord::Migration[5.1]
-  def change
+  class Link < ActiveRecord::Base
+  end
+
+  def up
     add_column :links, :moved_to_list_at, :datetime
+    Link.all.each do |link|
+      link.update!(moved_to_list_at: link.read_at.present? ? link.read_at : link.created_at)
+    end
+  end
+
+  def down
+    remove_column :links, :moved_to_list_at, :datetime
   end
 end
