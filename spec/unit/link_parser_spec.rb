659f00d6a19069c36e5727a7bd5b39492553d384
Josh Justice <jjustice@bignerdranch.com>
2017-12-04 08:21:58 -0500

    Reduce duplication in link spec

--- spec/unit/link_parser_spec.rb
+++ spec/unit/link_parser_spec.rb
@@ -3,45 +3,51 @@
 require 'link_parser'
 
 RSpec.describe LinkParser, :vcr do
-  subject(:link_parser) { LinkParser }
+  subject(:link) { described_class.process(url: url) }
 
   describe '#canonical' do
-    it 'returns the passed-in url when there are no redirects' do
-      url = 'http://codingitwrong.com'
-      canonical = link_parser.process(url: url).canonical
-      expect(canonical).to eq(url)
+    context 'when there are no redirects' do
+      let(:url) { 'http://codingitwrong.com' }
+      it 'returns the passed-in url' do
+        expect(link.canonical).to eq(url)
+      end
     end
 
-    it 'follows redirects' do
-      url = 'http://bit.ly/jay-is-great'
-      canonical = link_parser.process(url: url).canonical
-      expect(canonical).to eq('https://www.bignerdranch.com/blog/testing-external-dependencies-with-fakes/')
+    context 'when there are redirects' do
+      let(:url) { 'http://bit.ly/jay-is-great' }
+      it 'follows redirects' do
+        expect(link.canonical).to eq('https://www.bignerdranch.com/blog/testing-external-dependencies-with-fakes/')
+      end
     end
   end
 
   describe '#title' do
-    it 'loads the URL and returns the title tag text' do
-      url = 'http://codingitwrong.com/2017/07/24/letting-people-learn.html'
-      title = link_parser.process(url: url).title
-      expect(title).to eq('Letting People Learn - CodingItWrong')
+    context 'when there are no redirects' do
+      let(:url) { 'http://codingitwrong.com/2017/07/24/letting-people-learn.html' }
+      it 'loads the URL and returns the title tag text' do
+        expect(link.title).to eq('Letting People Learn - CodingItWrong')
+      end
     end
 
-    it 'follows redirects' do
-      url = 'https://google.com'
-      title = link_parser.process(url: url).title
-      expect(title).to eq('Google')
+    context 'when there are redirects' do
+      let(:url) { 'https://google.com' }
+      it 'follows redirects' do
+        expect(link.title).to eq('Google')
+      end
     end
 
-    it 'trims spaces' do
-      url = 'https://www.destroyallsoftware.com/talks/ideology'
-      title = link_parser.process(url: url).title
-      expect(title).to eq('Ideology')
+    context 'when there are spaces in the title' do
+      let(:url) { 'https://www.destroyallsoftware.com/talks/ideology' }
+      it 'trims spaces' do
+        expect(link.title).to eq('Ideology')
+      end
     end
 
-    it 'decodes entities' do
-      url = 'http://confreaks.tv/videos/rubyconf2017-keynote-you-re-insufficiently-persuasive'
-      title = link_parser.process(url: url).title
-      expect(title).to eq("Confreaks TV | Keynote: You're Insufficiently Persuasive - Ruby Conference 2017")
+    context 'when there are entities in the title' do
+      let(:url) { 'http://confreaks.tv/videos/rubyconf2017-keynote-you-re-insufficiently-persuasive' }
+      it 'decodes entities' do
+        expect(link.title).to eq("Confreaks TV | Keynote: You're Insufficiently Persuasive - Ruby Conference 2017")
+      end
     end
   end
 end
