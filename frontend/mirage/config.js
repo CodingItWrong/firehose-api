8c00598476fc2f33b0dd865bec3f11b61eefa2e6
Josh Justice <jjustice@bignerdranch.com>
2018-03-19 09:08:11 -0400

    Set up Ember and Rails to serve together

--- frontend/mirage/config.js
+++ frontend/mirage/config.js
@@ -1,5 +1,8 @@
 export default function() {
 
+  this.urlPrefix = '/api';
+  this.get('/links');
+
   // These comments are here to help you get started. Feel free to delete them.
 
   /*
@@ -8,7 +11,6 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
