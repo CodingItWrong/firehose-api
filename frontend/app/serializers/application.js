68364c302ce26c819ed0c69448f77a86724c6641
Josh Justice <jjustice@bignerdranch.com>
2018-03-19 13:31:46 -0400

    Display links in Ember app

---
+++ frontend/app/serializers/application.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  modelNameFromPayloadKey(payloadKey) {
+    if (payloadKey === 'my-links') {
+      return 'link';
+    }
+
+    return this._super(payloadKey);
+  }
+});
