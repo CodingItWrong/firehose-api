42186c06c45fbff2b2ba36afd0d7f843458d9bf7
Josh Justice <jjustice@bignerdranch.com>
2018-03-20 07:09:22 -0400

    Switch to running Ember and Rails normally on default ports

--- frontend/app/authenticators/oauth.js
+++ frontend/app/authenticators/oauth.js
@@ -1,5 +1,11 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import ENV from '../config/environment';
+
+const serverTokenPath = '/api/oauth/token';
+const serverTokenEndpoint = ENV.apiHost
+  ? ENV.apiHost + serverTokenPath
+  : serverTokenPath;
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: '/api/oauth/token',
+  serverTokenEndpoint,
 });
