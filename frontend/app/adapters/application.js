75cf31e701f4a1aed9cb647f8db6199015f6b586
Josh Justice <jjustice@bignerdranch.com>
2018-03-22 21:02:05 -0400

    Send auth token with model requests

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,12 +1,14 @@
 import DS from 'ember-data';
 import ENV from '../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 let options = {
   namespace: 'api',
+  authorizer: 'authorizer:oauth2',
 };
 
 if (ENV.apiHost) {
   options.host = ENV.apiHost;
 }
 
-export default DS.JSONAPIAdapter.extend(options);
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, options);
