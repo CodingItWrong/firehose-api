a4a655aafb474c27c231db03a2537d9b009c1a93
Josh Justice <jjustice@bignerdranch.com>
2018-03-24 09:51:47 -0400

    Add more fields to link model exposed

--- frontend/app/models/link.js
+++ frontend/app/models/link.js
@@ -3,4 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   url: DS.attr('string'),
+  comment: DS.attr('string'),
+  source: DS.attr('string'),
+  read: DS.attr('boolean'),
 });
