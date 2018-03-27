32c2a4135bc1acfe97c826f1f121e6bad864eb09
Josh Justice <jjustice@bignerdranch.com>
2018-03-27 07:56:28 -0400

    Enable creating links on backend

---
+++ spec/requests/add_link_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'list links', type: :request do
+  let!(:public_link) { FactoryBot.create(:link, :public) }
+  let!(:private_link) { FactoryBot.create(:link, :private) }
+
+  it 'creates a new link record' do
+    # TODO why does this succeed?
+    headers = {
+      'Authorization' => 'Bearer ',
+      'Content-Type' => 'application/vnd.api+json',
+    }
+    params = {
+      data: {
+        type: 'my_links',
+        attributes: {
+          url: 'https://www.example.com',
+        },
+      },
+    }
+    post '/api/links', headers: headers, params: params.to_json
+
+    expect(response.status).to eq(201)
+
+    jsonapi_response = JSON.parse(response.body)
+    link = jsonapi_response['data']
+    expect(link['id']).not_to be_nil
+  end
+end
