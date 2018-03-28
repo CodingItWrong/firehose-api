6289b2ec6fd23af1051a723c18596c3ef4407d4c
Josh Justice <jjustice@bignerdranch.com>
2018-03-28 06:46:29 -0400

    Create API endpoint to update links

--- spec/requests/managing_links_spec.rb
+++ spec/requests/managing_links_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe 'managing links', type: :request do
   let!(:private_link) { FactoryBot.create(:link, :private) }
 
   let(:token) { FactoryBot.create(:access_token).token }
-
-  it 'creates a new link record' do
-    headers = {
+  let(:headers) {
+    {
       'Authorization' => "Bearer #{token}",
       'Content-Type' => 'application/vnd.api+json',
     }
+  }
+
+  it 'creates a new link record' do
     params = {
       data: {
         type: 'my_links',
@@ -28,5 +30,27 @@ RSpec.describe 'managing links', type: :request do
     jsonapi_response = JSON.parse(response.body)
     link = jsonapi_response['data']
     expect(link['id']).not_to be_nil
+  end
+
+  it 'can update a link' do
+    link = FactoryBot.create(:link)
+
+    title = 'Updated Title'
+    params = {
+      data: {
+        type: 'my-links',
+        id: link.id,
+        attributes: {
+          title: title,
+        },
+      },
+    }
+    patch "/api/links/#{link.id}", headers: headers, params: params.to_json
+
+    expect(response.status).to eq(200)
+
+    jsonapi_response = JSON.parse(response.body)
+    link = jsonapi_response['data']
+    expect(link['attributes']['title']).to eq(title)
   end
 end
