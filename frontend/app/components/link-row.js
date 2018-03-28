6cfb737fcab1d7be2d4df0e7108b8cf980918dc9
Josh Justice <jjustice@bignerdranch.com>
2018-03-28 08:27:25 -0400

    Add showIfRead flag to component

--- frontend/app/components/link-row.js
+++ frontend/app/components/link-row.js
@@ -1,9 +1,17 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   session: service(),
 
   tagName: 'li',
   classNames: ['list-group-item', 'clearfix', 'h-entry'],
+
+  showLink: computed('link.read', function() {
+    if (typeof this.showIfRead === 'undefined') {
+      return true;
+    }
+    return this.showIfRead === this.get('link').get('read');
+  }),
 });
--- frontend/tests/integration/components/link-row-test.js
+++ frontend/tests/integration/components/link-row-test.js
@@ -59,4 +59,43 @@ describe('{{link-row}}', () => {
     expect(find('[data-test-button-mark-unread]')).to.exist;
     expect(find('[data-test-button-mark-read]')).not.to.exist;
   });
+
+  it('shows itself if the model read status matches showIfRead', async function() {
+    let link = EmberObject.create({
+      title: 'My Title',
+      url: 'https://www.example.com/page',
+      read: false,
+    });
+    this.set('link', link);
+
+    await render(hbs`{{link-row link=link showIfRead=false}}`);
+
+    expect(find('[data-test-link-title]')).to.exist;
+  });
+
+  it('hides itself if the model read status does not match showIfRead', async function() {
+    let link = EmberObject.create({
+      title: 'My Title',
+      url: 'https://www.example.com/page',
+      read: true,
+    });
+    this.set('link', link);
+
+    await render(hbs`{{link-row link=link showIfRead=false}}`);
+
+    expect(find('[data-test-link-title]')).to.not.exist;
+  });
+
+  it('shows itself if showIfRead is not set', async function() {
+    let link = EmberObject.create({
+      title: 'My Title',
+      url: 'https://www.example.com/page',
+      read: false,
+    });
+    this.set('link', link);
+
+    await render(hbs`{{link-row link=link}}`);
+
+    expect(find('[data-test-link-title]')).to.exist;
+  });
 });
