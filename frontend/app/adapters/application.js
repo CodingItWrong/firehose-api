5d320e07f26492d935b046c1b5157c97f60ca87d
Josh Justice <jjustice@bignerdranch.com>
2018-03-31 18:03:06 -0400

    Replace deprecated adapter with authorizer header code

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,10 +1,17 @@
 import DS from 'ember-data';
+import { inject as service } from '@ember/service';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from '../config/environment';
 
 let options = {
+  session: service('session'),
+
   namespace: 'api',
-  authorizer: 'authorizer:oauth2',
+
+  authorize(xhr) {
+    let { access_token } = this.get('session.data.authenticated');
+    xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+  },
 };
 
 if (ENV.apiHost) {
