78f4ab17356a1455fecbfaafb5f2cdba521c9b13
Josh Justice <jjustice@bignerdranch.com>
2018-03-31 21:08:32 -0400

    Reset add form when clicking out

--- frontend/app/controllers/links/new.js
+++ frontend/app/controllers/links/new.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  resetForm() {
+    this.set('url', '');
+  },
+
   actions: {
     async handleSave() {
       let link = this.store.createRecord('link', { url: this.url });
