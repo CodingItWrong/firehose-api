a399602a3e29b72f05ff8cd08f3466590686cd38
Josh Justice <jjustice@bignerdranch.com>
2018-04-07 12:41:57 -0400

    Add link detail component

---
+++ frontend/tests/integration/components/link-detail-test.js
@@ -0,0 +1,62 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupRenderingTest } from 'ember-mocha';
+import { render, find } from '@ember/test-helpers';
+import Service from '@ember/service';
+import EmberObject from '@ember/object';
+import hbs from 'htmlbars-inline-precompile';
+
+describe('{{link-detail}}', function() {
+  setupRenderingTest();
+
+  it('does not display action buttons when signed out', async function() {
+    let session = Service.extend({ isAuthenticated: () => false });
+    this.owner.register('service:session', session);
+
+    let link = EmberObject.create({
+      title: 'My Title',
+      url: 'https://www.example.com/page',
+    });
+    this.set('link', link);
+
+    await render(hbs`{{link-detail link=link session=session}}`);
+
+    expect(find('[data-test-button-mark-read]')).not.to.exist;
+    expect(find('[data-test-button-edit]')).not.to.exist;
+    expect(find('[data-test-button-delete]')).not.to.exist;
+  });
+
+  it('it displays action buttons when signed in', async function() {
+    let session = Service.extend({ isAuthenticated: () => true });
+    this.owner.register('service:session', session);
+
+    let link = EmberObject.create({
+      title: 'My Title',
+      url: 'https://www.example.com/page',
+    });
+    this.set('link', link);
+
+    await render(hbs`{{link-detail link=link}}`);
+
+    expect(find('[data-test-button-mark-read]')).to.exist;
+    expect(find('[data-test-button-edit]')).to.exist;
+    expect(find('[data-test-button-delete]')).to.exist;
+  });
+
+  it('it displays Mark Unread when read', async function() {
+    let session = Service.extend({ isAuthenticated: () => true });
+    this.owner.register('service:session', session);
+
+    let link = EmberObject.create({
+      title: 'My Title',
+      url: 'https://www.example.com/page',
+      read: true,
+    });
+    this.set('link', link);
+
+    await render(hbs`{{link-detail link=link}}`);
+
+    expect(find('[data-test-button-mark-unread]')).to.exist;
+    expect(find('[data-test-button-mark-read]')).not.to.exist;
+  });
+});
