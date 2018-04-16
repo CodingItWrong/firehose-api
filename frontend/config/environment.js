c53398b6275a6687de56e0966e3b9e5762a699d6
Josh Justice <jjustice@bignerdranch.com>
2018-04-15 20:24:35 -0400

    Fix mirage in dev

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -29,9 +29,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    // ENV['ember-cli-mirage'] = {
-    //   enabled: false
-    // }
+    ENV['ember-cli-mirage'] = {
+      enabled: false
+    }
 
     ENV.apiHost = 'http://localhost:3000';
   }
