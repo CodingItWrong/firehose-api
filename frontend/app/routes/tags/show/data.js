f65726397687ec90b31f5b3abce9d9aa0aac7dff
Josh Justice <jjustice@bignerdranch.com>
2018-05-25 20:16:50 -0400

    Fix tag show page

--- frontend/app/routes/tags/show/data.js
+++ frontend/app/routes/tags/show/data.js
@@ -3,6 +3,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     let { tag_id } = this.paramsFor('tags/show');
-    return this.store.findRecord('tag', tag_id, { include: 'bookmarks' });
+    return this.store.query(
+      q => q.findRecord({ type: 'tag', id: tag_id }),
+      { sources: { remote: { include: ['bookmarks'] } } },
+    );
   },
 });
