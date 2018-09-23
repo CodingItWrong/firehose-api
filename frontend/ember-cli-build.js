8da18e7f392aa874caa5580c47f02c7761d656c8
Josh Justice <me@codingitwrong.com>
2018-09-22 19:42:54 -0500

    Add ember-bootstrap

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
+      'importBootstrapCSS': true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
