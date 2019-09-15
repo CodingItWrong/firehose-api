2e8b76cc32dcd9768c90b50522391bdb78228678
Josh Justice <josh.justice@accesscfa.com>
2019-09-15 12:02:38 -0400

    Reload data when paginating

--- frontend/app/routes/links/read/data.js
+++ frontend/app/routes/links/read/data.js
@@ -2,6 +2,13 @@ import Route from '@ember/routing/route'
 import { action } from '@ember/object'
 
 export default class ReadLinksDataRoute extends Route {
+  queryParams = {
+    page: {
+      as: 'page',
+      refreshModel: true,
+    },
+  }
+
   model({ page }) {
     return this.store.query('bookmark', {
       include: 'tags',
