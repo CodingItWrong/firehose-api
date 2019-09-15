f9b4555b311b2339d3d57fc89dc711cf4d290d2c
Josh Justice <josh.justice@accesscfa.com>
2019-09-15 12:01:13 -0400

    Configure pagination on backend

---
+++ config/initializers/jsonapi_resources.rb
@@ -0,0 +1,9 @@
+JSONAPI.configure do |config|
+  config.default_paginator = :paged
+
+  config.default_page_size = 3
+  config.maximum_page_size = 20
+
+  config.top_level_meta_include_page_count = true
+  config.top_level_meta_page_count_key = :page_count
+end
