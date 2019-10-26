cc6e64278af79fd0d850431865696d3340348bbd
Josh Justice <josh.justice@accesscfa.com>
2019-10-26 15:41:56 -0400

    Fix auth header to work with fetch

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data'
 import { inject as service } from '@ember/service'
+import { computed } from '@ember/object'
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin'
 import ENV from '../config/environment'
 
@@ -8,10 +9,13 @@ let options = {
 
   namespace: 'api',
 
-  authorize(xhr) {
-    let { access_token } = this.get('session.data.authenticated')
-    xhr.setRequestHeader('Authorization', `Bearer ${access_token}`)
-  },
+  headers: computed('session.data.authenticated.access_token', function() {
+    const headers = {}
+    if (this.session.isAuthenticated) {
+      headers.Authorization = `Bearer ${this.session.data.authenticated.access_token}`
+    }
+    return headers
+  }),
 }
 
 if (ENV.apiHost) {
