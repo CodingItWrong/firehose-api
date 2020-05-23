86e2b3bb03d8b1a5eaed926e3c829d7c2b941443
Josh Justice <josh.justice@accesscfa.com>
2020-05-23 06:28:34 -0400

    Remove unnecessary parens from decorators

--- frontend/app/models/bookmark.js
+++ frontend/app/models/bookmark.js
@@ -1,13 +1,13 @@
 import Model, { attr, hasMany } from '@ember-data/model'
 
 export default class Bookmark extends Model {
-  @attr() title
-  @attr() url
-  @attr() comment
-  @attr() source
-  @attr() tag_list
-  @attr() read
-  @attr() public
+  @attr title
+  @attr url
+  @attr comment
+  @attr source
+  @attr tag_list
+  @attr read
+  @attr public
   @attr('date') moved_to_list_at
   @attr('date') published_at
 
--- frontend/app/models/tag.js
+++ frontend/app/models/tag.js
@@ -1,6 +1,6 @@
 import Model, { attr, hasMany } from '@ember-data/model'
 
 export default class Tag extends Model {
-  @attr() name
+  @attr name
   @hasMany('bookmark') bookmarks
 }
