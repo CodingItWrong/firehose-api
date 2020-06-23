c6a3e489a95a4b0f946a738d7fedfa69a8117e18
Josh Justice <josh.justice@accesscfa.com>
2020-06-23 07:02:38 -0400

    Fix pagination resetting

--- frontend/app/routes/links/read/data.js
+++ frontend/app/routes/links/read/data.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route'
-import { action } from '@ember/object'
 
 export default class ReadLinksDataRoute extends Route {
   queryParams = {
@@ -29,11 +28,5 @@ export default class ReadLinksDataRoute extends Route {
   setupController(controller) {
     super.setupController(...arguments)
     controller.setSearchTextFromQP()
-  }
-
-  @action
-  willTransition() {
-    this.controller.reset()
-    return true
   }
 }
