ed3aded88a3a2086e4a63e18e21e9627a5a399c7
Josh Justice <me@codingitwrong.com>
2020-08-01 12:57:54 -0400

    Remove qunit reference

--- frontend/tests/test-helper.js
+++ frontend/tests/test-helper.js
@@ -1,8 +1,5 @@
 import Application from 'firehose/app'
 import config from 'firehose/config/environment'
 import { setApplication } from '@ember/test-helpers'
-import { start } from 'ember-qunit'
 
 setApplication(Application.create(config.APP))
-
-start()
