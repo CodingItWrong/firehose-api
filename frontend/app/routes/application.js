763e653debe5db84d17f28009f23983f954f235c
Josh Justice <me@codingitwrong.com>
2020-08-17 09:04:15 -0400

    Remove unused jQuery import

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
-import $ from 'jquery';
 
 export default class ApplicationRoute extends Route {
   scrollToTop() {
