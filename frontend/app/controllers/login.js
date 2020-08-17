cbd544b9b57f4ff06215eb906f3a15d8a3fecb2d
Josh Justice <me@codingitwrong.com>
2020-08-17 08:48:18 -0400

    Switch to tracked property for error message

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginController extends Controller {
   @service session;
+
+  @tracked errorMessage;
 
   resetLoginForm() {
     this.setProperties({
@@ -20,12 +23,10 @@ export default class LoginController extends Controller {
       await this.session.authenticate('authenticator:oauth', email, password);
       await this.transitionToRoute('index');
     } catch (e) {
-      const errorMessage =
+      this.errorMessage =
         e?.responseJSON?.error_description ??
         e?.message ??
         'An error occurred while logging in.';
-
-      this.set('errorMessage', errorMessage);
     }
   }
 }
