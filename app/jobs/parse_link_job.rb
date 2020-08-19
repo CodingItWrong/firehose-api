a8fe6f2930ef070fa2731ddb5e97472f14d60b44
Josh Justice <me@codingitwrong.com>
2020-08-19 11:40:04 -0400

    Fix cops

--- app/jobs/parse_link_job.rb
+++ app/jobs/parse_link_job.rb
@@ -9,22 +9,27 @@ class ParseLinkJob < ApplicationJob
 
   def perform(link_params)
     parsed_link = link_parser.process(url: link_params[:url])
+    title = title_to_use(link_params, parsed_link)
 
-    attributes = { url: parsed_link.canonical }
-    attributes[:title] = if default_title?(link_params)
+    Link.create!(
+      url: parsed_link.canonical,
+      title: title,
+    )
+  end
+
+  private
+
+  def title_to_use(link_params, parsed_link)
+    if default_title?(link_params)
       begin
         parsed_link.title
-      rescue
+      rescue Cliver::Dependency::NotFound
         link_params[:title]
       end
     else
       link_params[:title]
     end
-
-    Link.create!(attributes)
   end
-
-  private
 
   def link_parser
     LinkParser
