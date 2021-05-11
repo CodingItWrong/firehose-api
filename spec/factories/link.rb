bef8640f04e98d4868984bfbc3d02d82401f509d
Josh Justice <me@codingitwrong.com>
2021-05-11 07:43:45 -0400

    Test Tag#used

--- spec/factories/link.rb
+++ spec/factories/link.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :link do
     sequence(:title) { |n| "Link #{n}" }
     url { 'https://example.com' }
+    association :tags
 
     trait :public do
       published_at { Time.now }
