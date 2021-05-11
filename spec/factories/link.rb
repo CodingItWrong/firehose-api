26e0888e9b9f38e46e535bc77ddc57eb650d4f3b
Josh Justice <me@codingitwrong.com>
2021-05-11 07:49:41 -0400

    Fix failing specs

--- spec/factories/link.rb
+++ spec/factories/link.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
   factory :link do
     sequence(:title) { |n| "Link #{n}" }
     url { 'https://example.com' }
-    association :tags
 
     trait :public do
       published_at { Time.now }
