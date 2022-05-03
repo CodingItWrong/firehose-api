d2a5e095ded5591fd6a7213d0bbe38a4ede81282
Josh Justice <me@codingitwrong.com>
2022-05-03 16:42:52 -0400

    Fix cops on env fetching

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -18,10 +18,10 @@ module TwitterClient
     def client
       @client ||=
         Twitter::REST::Client.new do |config|
-          config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-          config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-          config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-          config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+          config.consumer_key = ENV.fetch('TWITTER_CONSUMER_KEY')
+          config.consumer_secret = ENV.fetch('TWITTER_CONSUMER_SECRET')
+          config.access_token = ENV.fetch('TWITTER_ACCESS_TOKEN')
+          config.access_token_secret = ENV.fetch('TWITTER_ACCESS_TOKEN_SECRET')
         end
     end
   end
